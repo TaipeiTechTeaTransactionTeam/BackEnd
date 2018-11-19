6ff79a047fd4fcce50df241f75059396073aef1b
read0609 <uiouio123654@gmail.com>
2018-11-19 18:41:14 +0800

    django project init
    django project init

---
+++ TTTS/TTTS/TTTS/settings/server.py
@@ -0,0 +1,19 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ')(nnp=1cq*ty11n-75i8+@enp@xod_4qqem4*^p1u%47to2n+g'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR,'my.cnf'),
+        },
+    }
+}
