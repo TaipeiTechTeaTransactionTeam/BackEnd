12b7c2999db8d32b2bd2befc586a4611bd8138a0
read0609 <uiouio123654@gmail.com>
2018-11-19 19:38:13 +0800

    django project init
    因為DEGUG = False，ALLOWED_HOSTS =[]不能為空值，故修改base.py和server.py的ALLOWED_HOSTS

--- TTTS/TTTS/TTTS/settings/server.py
+++ TTTS/TTTS/TTTS/settings/server.py
@@ -6,6 +6,11 @@ SECRET_KEY = ')(nnp=1cq*ty11n-75i8+@enp@xod_4qqem4*^p1u%47to2n+g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+
+ALLOWED_HOSTS = ['http://35.234.8.158/','http://35.234.8.158/']
+
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
