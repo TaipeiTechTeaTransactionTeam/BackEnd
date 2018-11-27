96886fa2b20d7209cc8013486684a9d9ddf6195b
zxjte9411 <a5852241@gmail.com>
2018-11-28 00:27:05 +0800

    fix  LANGUAGE_CODE to 'en-us'

--- TTTS/settings/base.py
+++ TTTS/settings/base.py
@@ -83,7 +83,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hant'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Taipei'
 
