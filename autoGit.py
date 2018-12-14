75e20aab7e4249321f18bcb8d61d2448b7d6e3f6
NawaNawa <ltue2129@gmail.com>
2018-12-15 03:38:04 +0800

    完成自動Git註解

--- autoGit.py
+++ autoGit.py
@@ -1,3 +1,27 @@
+#發現Ｄｊａｎｇｏ開發環境本來就支援Ｐｙｔｈｏｎ，又寫了ｓｈ跟ｂａｔ的我，乾脆．．．．．．．．來個跨平台ＰＹ腳本
+
+#使用方法 
+    #python autoGit.py -參數(選填) commit名稱(選填)
+    #或
+    #python autoGit.py commit名稱(選填) -參數(選填)
+    
+    #可選Args說明
+        #可用參數輸入，不輸入程式執行會循問commitName
+        #減號參數不填=add+commit+push全做
+        
+    #參數 減開頭 (一般順序 add->commit->push)
+        # -a : add
+        # -c : commit
+        # -p : push
+        # 可以減號後方多個英文字母
+        # e.g. -acp,-ap,-cp...
+        # 第二碼為~或者-表示前面的動作也要做
+        # --p, -~p : -acp , add+commit+push
+        # --c, -~c : -ac , add+commit
+
+#注意
+    #會產生記錄檔branch.log紀錄是否切換branch，保障不亂推送到別人的分支
+
 from subprocess import getoutput
 from sys import argv 
 BranchCheckLogPath="./branch.log"
@@ -19,7 +43,10 @@ def commit():
         commitName="\""+commitName+"\""
         return commitName
     commitName=getCommitName()
-    print(getoutput("git commit -m "+commitName))
+    try:
+        print(getoutput("git commit -m "+commitName))
+    except:
+        pass
 def isChangeBranch(branch):
     lastBranch=branch
     try:
@@ -40,7 +67,10 @@ def push():
     if(isChangeBranch(branchName)):
         if(input("<Warning!!!> You change the different branch, are you sure that you want to push the changes to \""+branchName+"\" (y/n)\n> ") in ["n","N"]):
             return
-    print(getoutput("git push origin "+branchName))
+    try:
+        print(getoutput("git push origin "+branchName))
+    except:
+        pass
 def step(op):
     ops={"a":add,"c":commit,"p":push}
     ops[op]()
@@ -56,8 +86,8 @@ def argsJudge():
     if(finalSet==set("")):
         finalSet=set("acp")
     steps = [x for x in priority if x in finalSet]
-    print("steps : ",end='')
-    print(steps)
+    #print("steps : ",end='')
+   # print(steps)
     for op in steps:
         step(op)
 
