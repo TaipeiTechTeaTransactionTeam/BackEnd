0a95ae11ff9f0546cc8803a253859577ecf9ff20
read0609 <uiouio123654@gmail.com>
2018-12-18 05:31:39 -0800

    change FrontEnd to storeApp

---
+++ storeApp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'storeApp'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('userPanel/', views.userPanel, name='userPanel'),
+    path('search/', views.search, name='search'),
+    path('accountPanel/', views.accountPanel, name='accountPanel'),
+    path('report/', views.report, name='report'),
+    path('teas/', views.teas, name='teas'),
+    path('login/', views.login, name='login'),
+    path('regesiter/', views.regesiter, name='regesiter'),
+    path('contact/', views.contact, name='contact'),
+]
