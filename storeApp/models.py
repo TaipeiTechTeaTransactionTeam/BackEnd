1ab88781aa3c1d03fe472c649912666134adc6b7
zxjte9411 <a5852241@gmail.com>
2018-12-20 03:10:54 +0800

    在 product model 新增 get_absolute_url 函式

--- storeApp/models.py
+++ storeApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import django.utils.timezone as timezone
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -32,6 +32,9 @@ class product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=0, null=False)
     description = models.TextField(max_length=255, null=False)
     AddDate = models.DateField(default=timezone.now)
+
+    def get_absolute_url(self):
+        return reverse('storeApp:detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.name
