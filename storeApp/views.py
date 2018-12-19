ce61b811bd175db81d31161969327165c3ee87e0
zxjte9411 <a5852241@gmail.com>
2018-12-19 22:03:40 +0800

    修 bug

--- storeApp/views.py
+++ storeApp/views.py
@@ -14,9 +14,12 @@ def search(request):
 def userPanel(request):
     return render(request, 'storeApp/userPanel.html')
 
+def userSetting(request):
+    return render(request, 'storeApp/userSetting.html')
+
 def accountPanel(request):
     return render(request,'storeApp/accountPanel.html')
-    
+
 def report(request):
     return render(request,'storeApp/report.html')
 
@@ -31,3 +34,18 @@ def regesiter(request):
 
 def contact(request):
     return render(request, 'storeApp/contact.html')
+
+def checkout(request):
+    return render(request, 'storeApp/checkout.html')
+
+def detail(request):
+    return render(request, 'storeApp/detail.html')
+
+def editProduct(request):
+    return render(request, 'storeApp/editProduct.html')
+
+def manageOrder(request):
+    return render(request, 'storeApp/manageOrder.html')
+
+def manageProductAndDiscount(request):
+    return render(request, 'storeApp/manageProductAndDiscount.html')
