552e08fc47429432739d9200786c85f385caba2a
read0609 <uiouio123654@gmail.com>
2018-12-22 22:43:12 +0800

    new store frieght

--- storeApp/models.py
+++ storeApp/models.py
@@ -10,6 +10,7 @@ class store(models.Model):
     name = models.CharField(max_length=255, null=False)
     address = models.CharField(max_length=255, null=False)
     phone = models.CharField(max_length=10, null=False, unique=True)
+    freight = models.DecimalField(max_digits=10, decimal_places=0, null=False, default = 0)
 
     def __str__(self):
         return self.name
