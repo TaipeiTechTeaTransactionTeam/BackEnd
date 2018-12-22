23dbb6446e6535c0c3c89f8209c426f831e86415
zxjte9411 <a5852241@gmail.com>
2018-12-22 20:50:56 +0800

    使用裝飾器來註冊 admin 要管理的東西

--- storeApp/admin.py
+++ storeApp/admin.py
@@ -1,27 +1,38 @@
 from django.contrib import admin
-from .models import product, store, teaType,SeasoningDiscount,ShippingDiscount,productDiscount
-# Register your models here.
+from .models import product
+from .models import store
+from .models import teaType
+from .models import SeasoningDiscount
+from .models import ShippingDiscount
+from .models import productDiscount
 
+
+@admin.register(product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name','teaType','image','amount','price','description','AddDate')
-    fields = ('name','teaType','image','amount','price','description')
+    list_display = ('name', 'teaType', 'image', 'amount',
+                    'price', 'description', 'AddDate')
+    fields = ('name', 'teaType', 'image', 'amount', 'price', 'description')
     ordering = ('AddDate',)
 
+
+@admin.register(product)
 class TeaTypeAdmin(admin.ModelAdmin):
-    list_display = ('name','image')
+    list_display = ('name', 'image')
 
+
+@admin.register(SeasoningDiscount)
 class SeasoningDiscountAdmin(admin.ModelAdmin):
-    list_display = ('id','discount','start_date','end_date')
+    list_display = ('id', 'discount', 'start_date', 'end_date')
 
+
+@admin.register(ShippingDiscount)
 class ShippingDiscountAdmin(admin.ModelAdmin):
-    list_display = ('id','discount','condition','start_date','end_date')
+    list_display = ('id', 'discount', 'condition', 'start_date', 'end_date')
 
+
+@admin.register(productDiscount)
 class ProductDiscountAdmin(admin.ModelAdmin):
-    list_display = ('id','discount','product','start_date','end_date')
+    list_display = ('id', 'discount', 'product', 'start_date', 'end_date')
 
-admin.site.register(product,ProductAdmin)
+
 admin.site.register(store)
-admin.site.register(teaType,TeaTypeAdmin)
-admin.site.register(SeasoningDiscount,SeasoningDiscountAdmin)
-admin.site.register(ShippingDiscount,ShippingDiscountAdmin)
-admin.site.register(productDiscount,ProductDiscountAdmin)
