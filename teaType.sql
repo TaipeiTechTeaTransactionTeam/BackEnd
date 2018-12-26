ca56925b23177136268081b2667024c818d9ae2b
yuki <love11662255@gmail.com>
2018-12-27 00:19:59 +0800

    測試資料

--- teaType.sql
+++
@@ -1,4 +0,0 @@
-USE TTTS;
-insert into storeApp_teatype VALUES(1,'紅茶','teaType/紅茶.jpg');
-insert into storeApp_teatype VALUES(2,'綠茶','teaType/綠茶.jpg');
-insert into storeApp_teatype VALUES(3,'烏龍茶','teaType/烏龍茶.jpg');
