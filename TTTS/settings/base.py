c6bfa5681d086a7e52bd6dfc2a7b9d6c1950b1ac
zxjte9411 <a5852241@gmail.com>
2018-12-31 02:15:46 +0800

    移除無用 app 等相關東西

--- TTTS/settings/base.py
+++ TTTS/settings/base.py
@@ -29,11 +29,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'pages',
     'base',
     'storeApp',
     'crispy_forms',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
